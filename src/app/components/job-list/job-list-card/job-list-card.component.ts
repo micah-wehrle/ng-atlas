925d3349197b3f889070ba7517ce12b3f99dc660
micah.wehrle <micah23@gmail.com>
2024-10-16 23:32:40 -0400

    Added basic popup pokemon card

--- src/app/components/job-list/job-list-card/job-list-card.component.ts
+++ src/app/components/job-list/job-list-card/job-list-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { JobData } from 'src/app/models/jobs-response.model';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-job-list-card',
@@ -14,13 +15,17 @@ export class JobListCardComponent implements OnInit {
   public showPopup: boolean = false;
   private deleteLaterButPopupTimerId;
 
-  constructor() { }
+  constructor(private readonly pokemonService: PokemonService) { }
 
   ngOnInit(): void {
   }
 
   public onPokeballClick($event: Event): void {
     $event.stopPropagation();
+
+    // console.log(this.job.pokemon.sprites);
+
+    this.pokemonService.spotPokemon(this.job.pokemon);
     
     if (this.showPopup) {
       this.showPopup = false;
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+import { Pokemon } from '../models/pokemon.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private pokemonData: PokemonCollection = {};
+
+  constructor() { }
+
+  public spotPokemon(pokemon: Pokemon): void {
+    if (this.pokemonData[pokemon.id]) {
+      return;
+    }
+
+    this.pokemonData[pokemon.id] = {
+      name: pokemon.name,
+      sprite: pokemon.sprites,
+      caught: false,
+    }
+  }
+
+  public catchPokemon(pokemon: Pokemon): void {
+    if (!this.pokemonData[pokemon.id]) {
+      this.spotPokemon(pokemon);
+    }
+
+    this.pokemonData[pokemon.id].caught = true;
+  }
+}
+
+export type PokemonCollection = {
+  [key: number]: {
+    name: string, 
+    sprite: {[key: string]: string}, 
+    caught: boolean
+  }
+}
